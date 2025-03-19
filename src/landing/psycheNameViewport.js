6e3117c193b35b8a3c8aa55f2c47e4be53b8327e
ArmandoA88 <armando.arratiad@gmail.com>
2025-03-19 18:48:45 -0500

    Fix recursive iFrame loading issue in psycheNameViewport.js

--- src/landing/psycheNameViewport.js
+++ src/landing/psycheNameViewport.js
@@ -76,7 +76,7 @@ export function showPsycheNameViewport() {
     
     // Create iframe to load the psycheName content
     iframe = document.createElement('iframe');
-    iframe.src = 'public/psycheName/psycheName.html';
+    iframe.src = 'public/psycheName/public/name/psycheName.html';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.border = 'none';

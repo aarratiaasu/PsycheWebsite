07c63fbeb2186c1d13afd2122f93bcf73fe02237
janselm <janselm@asu.edu>
2025-03-18 21:55:50 -0400

    changed directional lighting on asteroid model

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -72,8 +72,8 @@ function init() {
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(0, 0, 5);
+  const directionalLight = new THREE.DirectionalLight(0x7a5f3e, 10);
+  directionalLight.position.set(1, 1, 5);
   scene.add(directionalLight);
 
   // section tracking - handles the camera's moveTo function
